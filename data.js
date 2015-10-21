f98dfc9aedeee04ff234bb83210cf5e1df8f5de6
Galdopigen <galdopigen@gmail.com>
2015-10-21 14:59:46 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,144 @@
+var data = { "photos": [{ 	
+	"marker": "marker0", "latitude": 53.403975, "longitude": -6.219329 // clonshaugh <---- Irish Cafes
+},{
+	"marker": "marker1", "latitude": 53.427509, "longitude": -6.244545 // dublinAirportTerminal1
+},{
+	"marker": "marker2", "latitude": 53.425770, "longitude": -6.240629 // dublinAirportTerminal2
+},{
+	"marker": "marker3", "latitude": 53.392320, "longitude": -6.389717 // blanchardstownCentre
+},{
+	"marker": "marker4", "latitude": 53.340932, "longitude": -6.261892 // chathamStreet
+},{
+	"marker": "marker5", "latitude": 52.639987, "longitude": -8.645371 // limerick
+},{
+	"marker": "marker6", "latitude": 53.286408, "longitude": -6.240968 // dundrum
+},{
+	"marker": "marker7", "latitude": 53.340093, "longitude": -6.260537 // graftonStreet
+},{
+	"marker": "marker8", "latitude": 53.349807, "longitude": -6.260741 // henryStreet
+},{
+	"marker": "marker9", "latitude": 53.346480, "longitude": -6.292797 // heustonStation
+},{
+	"marker": "marker10", "latitude": 53.348124, "longitude": -6.265609 // jervis
+},{
+	"marker": "marker11", "latitude": 53.347053, "longitude": -6.263113 // liffeyStreet
+},{
+	"marker": "marker12", "latitude": 53.342522, "longitude": -6.256189 // nassauStreet
+},{
+	"marker": "marker13", "latitude": 53.284416, "longitude": -6.154890 // oliverPlunkett
+},{
+	"marker": "marker14", "latitude": 53.327708, "longitude": -6.258084 // ranelagh
+},{
+	"marker": "marker15", "latitude": 53.454368, "longitude": -6.219045 // swords
+},{
+	"marker": "marker16", "latitude": 53.342961, "longitude": -6.261904 // wicklowStreet
+},{
+	"marker": "marker17", "latitude": 53.273425, "longitude": -9.051940 // williamStreet
+},{
+	"marker": "marker18", "latitude": -36.913865, "longitude": 174.841132 // auckland <---- Franchise
+},{
+	"marker": "marker19", "latitude": -41.209808, "longitude": 174.906650 // lowerHutt
+},{
+	"marker": "marker20", "latitude": -40.916967, "longitude": 175.005329 // paraparaumu
+},{
+	"marker": "marker21", "latitude": -41.137410, "longitude": 174.839630 // porirua
+},{
+	"marker": "marker22", "latitude": -41.289014, "longitude": 174.774941 // wellington
+},{
+	"marker": "marker23", "latitude": 24.802008, "longitude": 67.030266 // dolmenMall
+},{
+	"marker": "marker24", "latitude": 31.506587, "longitude": 74.349236 // fortuneMall
+},{
+	"marker": "marker25", "latitude": 24.897640, "longitude": 67.169329 // karachiAirport
+},{
+	"marker": "marker26", "latitude": 24.817316, "longitude": 67.041087 // zamzama
+},{
+	"marker": "marker27", "latitude": 25.250943, "longitude": 55.350266 // dubaiAirport
+},{
+	"marker": "marker28", "latitude": 23.130782, "longitude": 90.124625 // bangladesh	
+},{
+	"marker": "marker29", "latitude": 24.415938, "longitude": 54.542742 // abuDhabi <---- Countries
+},{
+	"marker": "marker30", "latitude": -27.124293, "longitude": 143.470205 // australia
+},{
+	"marker": "marker29", "latitude": 26.121117, "longitude": 50.557553 // bahrain
+},{
+	"marker": "marker30", "latitude": -3.521126, "longitude": 29.793433 // burundi
+},{
+	"marker": "marker29", "latitude": 48.663855, "longitude": -75.609375 // canada
+},{
+	"marker": "marker30", "latitude": 28.735465, "longitude": 115.210330 // china
+},{
+	"marker": "marker29", "latitude": 25.136036, "longitude": 55.257310 // dubai
+},{
+	"marker": "marker30", "latitude": 27.402916, "longitude": 30.026617 // egypt
+},{
+	"marker": "marker29", "latitude": 52.370905, "longitude": -0.303274 // england
+},{
+	"marker": "marker30", "latitude": 62.583701, "longitude": 25.935419 // finland
+},{
+	"marker": "marker29", "latitude": 51.821717, "longitude": 11.245135 // germany
+},{
+	"marker": "marker30", "latitude": 22.269699, "longitude": 114.206849 // hongKong
+},{
+	"marker": "marker29", "latitude": 19.917039, "longitude": 79.878833 // india
+},{
+	"marker": "marker30", "latitude": -1.077536, "longitude": 115.729707 // indonesia
+},{
+	"marker": "marker29", "latitude": 37.290560, "longitude": 140.176471 // japan
+},{
+	"marker": "marker30", "latitude": 0.775613, "longitude": 38.501620 // kenya
+},{
+	"marker": "marker29", "latitude": 33.684075, "longitude": 35.671488 // lebanon
+},{
+	"marker": "marker30", "latitude": 2.885088, "longitude": 114.397201 // malaysia
+},{
+	"marker": "marker29", "latitude": -16.649367, "longitude": 37.219230 // mozambique
+},{
+	"marker": "marker30", "latitude": 52.651728, "longitude": 6.081372 // netherlands
+},{
+	"marker": "marker29", "latitude": -43.549920, "longitude": 170.444786 // newZealand
+},{
+	"marker": "marker30", "latitude": 20.260309, "longitude": 57.149139 // oman
+},{
+	"marker": "marker29", "latitude": 28.257081, "longitude": 67.484620 // pakistan
+},{
+	"marker": "marker30", "latitude": 15.360879, "longitude": 121.105641 // philippines
+},{
+	"marker": "marker29", "latitude": 52.621435, "longitude": 20.079203 // poland
+},{
+	"marker": "marker30", "latitude": 40.034113, "longitude": -7.941551 // portugal
+},{
+	"marker": "marker29", "latitude": 25.514395, "longitude": 51.276310 // qatar
+},{
+	"marker": "marker30", "latitude": 61.520346, "longitude": 105.373047 // russia
+},{
+	"marker": "marker29", "latitude": -1.876403, "longitude": 29.962833 // rwanda
+},{
+	"marker": "marker30", "latitude": 24.577009, "longitude": 45.261657 // saudiArabia
+},{
+	"marker": "marker29", "latitude": 56.807026, "longitude": -3.720200 // scotland
+},{
+	"marker": "marker30", "latitude": 25.212889, "longitude": 55.787508 // sharjah
+},{
+	"marker": "marker30", "latitude": 1.311127, "longitude": 103.849073 // singapore
+},{
+	"marker": "marker29", "latitude": -30.079528, "longitude": 24.694264 // southAfrica
+},{
+	"marker": "marker30", "latitude": 40.384466, "longitude": -2.702258 // spain
+},{
+	"marker": "marker29", "latitude": 7.781982, "longitude": 80.810206 // sriLanka
+},{
+	"marker": "marker30", "latitude": 63.803855, "longitude": 17.565257 // sweeden
+},{
+	"marker": "marker29", "latitude": 23.671400, "longitude": 121.048568 // taiwan
+},{
+	"marker": "marker30", "latitude": 15.582887, "longitude": 102.369010 // thailand
+},{
+	"marker": "marker29", "latitude": 36.742096, "longitude": -83.536094 // usa
+},{
+	"marker": "marker30", "latitude": 52.329864, "longitude": -3.632175 // wales
+},{
+	"marker": "marker29", "latitude": -14.883094, "longitude": 28.799015 // zambia
+}
+]};
